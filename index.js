304021b70bcf80a8db79880b3a4e61d4e6b2f52d
rafiaksd <76418474+rafiaksd@users.noreply.github.com>
2025-03-25 15:29:40 +0600

    Update CORS with PUT DELETE.js

--- index.js
+++ index.js
@@ -15,16 +15,16 @@ require('dotenv').config();
 const salt = bcrypt.genSaltSync(10)
 const mongoURI = process.env.MONGO_CONNECT_URI;
 const jwtSecret = process.env.JWT_SECRET
+
 const PORT = process.env.PORT
 
 const corsOptions = {
-     credentials: true,
-     origin: process.env.NODE_ENV === 'production' 
-       ? 'https://rafiaksd.github.io'  // Production URL
-       : 'http://localhost:5173',            // Local development URL
-   };
-   
+  origin: 'https://rafiaksd.github.io', // The GitHub Pages URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed methods
+  credentials: true, // Allow cookies to be sent with requests
+};
 app.use(cors(corsOptions));
+
 app.use(express.json())
 app.use(cookieParser())
 
